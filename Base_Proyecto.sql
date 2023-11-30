784138ecaf105fc95aa9e0497607833aa8d71934
mariod12 <73258801+mariod12@users.noreply.github.com>
2023-11-30 00:10:57 -0600

    Update Base_Proyecto.sql

--- Base_Proyecto.sql
+++ Base_Proyecto.sql
@@ -3,70 +3,99 @@ GO
 
 USE Telecomunication_Bussines;
 GO
-
+--esquemas 
 CREATE SCHEMA Personas
 GO
-
 CREATE SCHEMA Clientes
 GO
-
 CREATE SCHEMA Empleados
 GO
-
 CREATE SCHEMA Ventas
 GO
+--create tables 
+CREATE TABLE Personas.Paises(
+idPais INTEGER IDENTITY (1,1) PRIMARY KEY,
+descripcion VARCHAR (20) NOT NULL
+);
 
-CREATE TABLE Personas.genero(
-id_genero INT IDENTITY PRIMARY KEY,
-descripcion NVARCHAR (50));
-GO
+CREATE TABLE Personas.Departamentos(
+idDepto INTEGER IDENTITY(1,1) PRIMARY KEY, 
+idPais INTEGER NOT NULL,
+descripcion VARCHAR (50)
+CONSTRAINT FK_depto_pais FOREIGN KEY (idPais) REFERENCES Personas.Paises(idPais)
+);
 
-CREATE TABLE Personas.departamento(
-id_departamento INT IDENTITY PRIMARY KEY,
-descripcion VARCHAR(100));
-GO
+CREATE TABLE Personas.Municipios(
+idMuncipio INTEGER IDENTITY (1,1) PRIMARY KEY, 
+idDepto INTEGER NOT NULL,
+decripcion VARCHAR(50)
+CONSTRAINT FK_mun_depto FOREIGN KEY (idDepto) REFERENCES Personas.Departamentos(idDepto)
+);
 
-CREATE TABLE Personas.municipio(
-id_municipio INT IDENTITY PRIMARY KEY,
-descripcion VARCHAR(100));
-GO
 
-CREATE TABLE Personas.colonia(
-id_colonia INT IDENTITY PRIMARY KEY,
-descripcion VARCHAR(100)
+CREATE TABLE Personas.Colonias(
+id_colonia INT IDENTITY (1,1) PRIMARY KEY,
+id_municipio int not null, 
+descripcion VARCHAR(100),
+CONSTRAINT FK_colonia FOREIGN KEY (id_municipio) REFERENCES Personas.Municipios(idMuncipio)
 );
 GO
 
-CREATE TABLE Personas.Personas (
-    ID_persona NVARCHAR(50) PRIMARY KEY,
-    p_nombre NVARCHAR(50),
-	s_nombre NVARCHAR(50),
-    p_apellido NVARCHAR(50),
-	s_apellido NVARCHAR(50),
-    Direccion NVARCHAR(100),
-	fecha_nacimiento date,
-    Numero_de_telefono NVARCHAR(15),
-	correo_electronico NVARCHAR(100)
-	id_colonia INT,
-	id_genero INT
+CREATE TABLE personas.Generos(
+id_genero INTEGER IDENTITY PRIMARY KEY,
+descripcion VARCHAR(20) NOT NULL,
 );
-GO
 
-
-CREATE TABLE Clientes.Clientes (
-    ID_Cliente INT PRIMARY KEY,
-    ID_persona NVARCHAR(50),
-	fecha_afiliaccion DATE, 
-	id_tipo_cliente INT,
-
+CREATE TABLE Personas.Personas(
+idPersona INTEGER IDENTITY(1,1) PRIMARY KEY, 
+id_colonia INTEGER NOT NULL,
+id_genero INTEGER NOT NULL,	
+P_nombre VARCHAR (25) NOT NULL, 
+S_nombre VARCHAR (25) NULL,
+P_apellido VARCHAR (25) NOT NULL,
+S_apellido VARCHAR (25) NULL, 
+direccion varchar (100), 
+telefono varchar (10),
+fecha_nacimiento DATE NULL,
+correo VARCHAR (100) NOT NULL,
+CONSTRAINT FK_per_mun FOREIGN KEY (id_colonia) REFERENCES  Personas.Colonias(id_colonia),
+CONSTRAINT FK_per_gen FOREIGN KEY (id_genero) REFERENCES Personas.Generos(id_genero)
 );
-GO
+
 
 CREATE TABLE Clientes.tipoCliente(
   id_tipo_cliente INT IDENTITY PRIMARY KEY,
   descripcion NVARCHAR(100),
   beneficios NVARCHAR(100) );
   GO
+
+
+CREATE TABLE Clientes(
+id_clientes INTEGER IDENTITY(1,1) PRIMARY KEY, 
+id_persona INTEGER NOT NULL,
+id_tipo_cliente INT not null,
+fecha_afiliaccion DATE, 
+CONSTRAINT Fk_tipo_cliente FOREIGN KEY (id_tipo_cliente) REFERENCES Clientes.tipoCliente(id_tipo_cliente),
+CONSTRAINT Fk_persona FOREIGN KEY (id_tipo_cliente) REFERENCES Personas.Personas(idPersona),
+
+);
+
+CREATE TABLE Empleados.PuestosTrabajo(
+idPuesto INTEGER IDENTITY (1,1) PRIMARY KEY,
+descripcion VARCHAR (80) NOT NULL,
+comentario VARCHAR (150) NULL
+);
+
+
+CREATE TABLE Empleados.Empleados (
+id_empleado INTEGER PRIMARY KEY,
+id_persona INTEGER NOT NULL,
+id_puesto INTEGER NOT NULL, 
+Salario DECIMAL(10, 2),
+Fecha_de_contratacion DATE,
+CONSTRAINT FK_emp_per FOREIGN KEY (id_persona) REFERENCES personas.Personas(idPersona),
+CONSTRAINT FK_emp_puesto FOREIGN KEY (id_puesto) REFERENCES empleados.PuestosTrabajo(idPuesto),
+);
 
 ---(productos: Telefonia fija, telefonia movil...)
 CREATE TABLE Ventas.Servicios(
@@ -149,7 +178,7 @@ CREATE TABLE Servicios.Soporte (
 GO
 
 
-CREATE TABLE Residencial.Facturacion (
+CREATE TABLE ventas.Facturacion (
     ID_Factura INT PRIMARY KEY,
     ID_Cliente INT,
 	ID_Servicio INT,
@@ -159,15 +188,33 @@ CREATE TABLE Residencial.Facturacion (
 );
 GO
 
+CREATE TABLE Factura(
+	n_Factura INTEGER PRIMARY KEY,
+	id_empleado INTEGER NOT NULL,
+	id_cliente INTEGER,
+	fecha_hora DATETIME NOT NULL,
+	id_local INTEGER NOT NULL,
+	monto_pagado DECIMAL (10,2) NOT NULL,
+	cambio DECIMAL (10,2) NOT NULL,
+	sub_total DECIMAL (10,2) NOT NULL,
+	gravado15 DECIMAL (10,2) NOT NULL,
+	total DECIMAL (10,2) NOT NULL,
+	CONSTRAINT FK_id_empleado FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado),
+	CONSTRAINT FK_id_cliente FOREIGN KEY (id_cliente) REFERENCES Clientes(id_clientes),
+	CONSTRAINT FK_id_local FOREIGN KEY (id_local) REFERENCES Sucursales(idlocal)
+	);
 
-CREATE TABLE Empleados (
-    ID_Empleado INT PRIMARY KEY,
-    Nombre NVARCHAR(50),
-    Apellido NVARCHAR(50),
-    Cargo NVARCHAR(50),
-    Fecha_de_contratacion DATE
-);
-GO
+CREATE TABLE detalle_compra(
+	id_detalleCompra INTEGER PRIMARY KEY,
+	n_factura INTEGER NOT NULL,
+	producto VARCHAR(20) NOT NULL,
+	cantidad INTEGER NOT NULL,
+	CONSTRAINT FK_n_factura FOREIGN KEY (n_factura) REFERENCES Factura(n_factura),
+	CONSTRAINT FK_Producto FOREIGN KEY (producto) REFERENCES Productos(codigo_barras)
+	);
+ALTER TABLE detalle_compra
+ADD PrecioProducto DECIMAL(10,2)
+
 
 ---DIMENSIONES 
 ---tiempo
